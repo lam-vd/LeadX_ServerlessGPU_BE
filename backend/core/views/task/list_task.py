ef48d559cf6dc236d93b5d51fde6b679303e63bf
lam-vd <lamvd@zen-s.com>
2025-04-09 13:26:57 +0700

    Improvement API list task swagger

--- backend/core/views/task/list_task.py
+++ backend/core/views/task/list_task.py
@@ -1,10 +1,11 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
 from core.models.task import Task
 from core.serializers.task import TaskSerializer
 from django.db.models import Count, Q
 from core.swagger.task_list_swagger import task_list_swagger
-from core.utils.response_formatter import success_response
+from core.utils.response_formatter import success_response, error_response
 
 class TaskListView(generics.ListAPIView):
     serializer_class = TaskSerializer
@@ -20,9 +21,14 @@ class TaskListView(generics.ListAPIView):
         ).order_by('-created_at')
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        page = self.paginate_queryset(queryset)
-        return self.format_paginated_response(page) if page else self.format_full_response(queryset)
+        try:
+            queryset = self.get_queryset()
+            page = self.paginate_queryset(queryset)
+            return self.format_paginated_response(page) if page else self.format_full_response(queryset)
+        except NotFound as e:
+            return error_response(errors=str(e), message="invalid_page_number", status_code=404)
+        except Exception as e:
+            return error_response(errors=str(e), message="an_unexpected_error_occurred", status_code=500)
 
     def format_paginated_response(self, page):
         serializer = self.get_serializer(page, many=True)
